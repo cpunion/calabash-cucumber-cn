c029d98a074c8a7ef8ba9ee4f2428130f7d7ddbe
李杰 <lijie@iyunyao.com>
2014-05-06 11:42:20 +0800

    modify version number to 0.0.2

--- lib/calabash-cucumber-ios-cn/version.rb
+++ lib/calabash-cucumber-ios-cn/version.rb
@@ -2,8 +2,8 @@ module Calabash
   module Cucumber
     module IOS
       module CN
-        VERSION = '0.0.1'
-        FRAMEWORK_VERSION = '0.0.1'
+        VERSION = '0.0.2'
+        FRAMEWORK_VERSION = '0.0.2'
       end
     end
   end
