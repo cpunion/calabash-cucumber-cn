f81404d108df580e2af79276d162c3e773ba5ec4
李杰 <lijie@iyunyao.com>
2014-05-07 19:23:47 +0800

    release 0.0.5

--- lib/calabash-cucumber-ios-cn/version.rb
+++ lib/calabash-cucumber-ios-cn/version.rb
@@ -2,8 +2,8 @@ module Calabash
   module Cucumber
     module IOS
       module CN
-        VERSION = '0.0.4'
-        FRAMEWORK_VERSION = '0.0.4'
+        VERSION = '0.0.5'
+        FRAMEWORK_VERSION = '0.0.5'
       end
     end
   end
