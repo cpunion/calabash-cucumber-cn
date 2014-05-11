c1a9a360a4de2ee2257523f6a189c149359c2058
李杰 <lijie@iyunyao.com>
2014-05-11 15:34:28 +0800

    release 0.0.7

--- lib/calabash-cucumber-cn/version.rb
+++ lib/calabash-cucumber-cn/version.rb
@@ -1,8 +1,8 @@
 module Calabash
   module Cucumber
     module CN
-      VERSION = '0.0.6'
-      FRAMEWORK_VERSION = '0.0.6'
+      VERSION = '0.0.7'
+      FRAMEWORK_VERSION = '0.0.7'
     end
   end
 end
